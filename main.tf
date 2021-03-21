fe2440723659beda6951942519ea01ee5a62d8b6
Erik Anderson <erikanderson753@gmail.com>
2021-03-21 15:10:50 -0400

    Adding t2 micro instance

--- main.tf
+++ main.tf
@@ -19,6 +19,52 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "example" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-2c"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "interface" {
+  subnet_id   = aws_subnet.main.id
+  private_ips = ["10.0.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "main" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.interface.id
+    device_index         = 0
+  }
+
 }
